17bbaf6e43d4cddd8afffe5f60d07a6aaa33f4b8
przemskydev <przemekgithub@o2.pl>
2020-11-25 18:24:26 +0100

    adding view engine and created hbs files in view folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 const path = require('path')
 const express = require('express')
+// const hbs = require('hbs')
 
 const app = express()
 const port = 3000
 const publicDir = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDir))
+
+app.get('/', (req,res)=>{
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Brzemeg XD'
+  })
+})
+
+app.get('/about', (req,res)=>{
+  res.render('about', {
+    title: 'Weather App',
+    name: 'Brzemeg XD'
+  })
+})
+
+app.get('/help', (req,res)=>{
+  res.render('help', {
+    title: 'Weather app help page.',
+    name: 'Brzemeg XD'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
