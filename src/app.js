e2bd746111ac04c6ab0644b734bf71665656e98c
przemskydev <przemekgithub@o2.pl>
2020-11-26 14:27:04 +0100

    add partials and 404 view

--- src/app.js
+++ src/app.js
@@ -1,39 +1,44 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const port = 3000
 
 // Define paths for express config
 const publicDir = path.join(__dirname, '../public')
-const viewPath = path.join(__dirname, '../templates')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewPath)
+hbs.registerPartials(partialPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDir))
 
 //Routes
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'Weather App',
     name: 'Brzemeg XD'
   })
 })
 
-app.get('/about', (req,res)=>{
+app.get('/about', (req, res) => {
   res.render('about', {
-    title: 'Weather App',
+    title: 'About - Weather App',
     name: 'Brzemeg XD'
   })
 })
 
-app.get('/help', (req,res)=>{
+app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Weather app help page.',
-    name: 'Brzemeg XD'
+    name: 'Brzemeg XD',
+    title: 'Help',
+    helpText: 'Some helpful text'
   })
 })
 
@@ -42,6 +47,22 @@ app.get('/weather', (req, res) => {
     temperature: 5,
     feelsLike: 2,
     location: 'Lublin'
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404 page not found',
+    message: 'Help article not found',
+    name: 'Brzemeg XD'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404 page not found',
+    message: 'Page not found',
+    name: 'Brzemeg XD'
   })
 })
 
