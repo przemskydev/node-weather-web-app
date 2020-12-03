cfef5e27a0b1342e6fcb8c68b546bf5a7638e31a
przemskydev <przemekgithub@o2.pl>
2020-12-03 20:37:33 +0100

    added styles for input and button

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,25 @@
-console.log('client side js')
-
-
-fetch('http://localhost:3000/weather?address=Lublin')
-  .then((response) => {
-    response.json()
-      .then((data) => {
-        if (data.error) return console.log(data.error)
-        console.log(data.location)
-        console.log(data.forecast)
-      })
-  })
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('.message-1')
+const messageTwo = document.querySelector('.message-2')
 
-weatherForm.addEventListener('submit', (e)=>{
+weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
   const location = search.value
 
-  console.log(location)
+  messageOne.textContent="Loading forecast data..."
+  messageTwo.textContent=""
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          if (data.error) return messageOne.textContent=data.error
+          messageOne.textContent=data.location
+          messageTwo.textContent=data.forecast
+        })
+    })
+
 })
 
