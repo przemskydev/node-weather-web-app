3a3414cf307ec9ac4c14ad91bb07980040bf01be
przemskydev <przemekgithub@o2.pl>
2020-12-06 20:10:06 +0100

    add more data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,14 +5,17 @@ const forecast = (lat, long, callback) => {
 
   const url = `http://api.weatherstack.com/current?access_key=${key}&query=${lat},${long}`
 
-  request({url, json: true }, (error, { body }) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connection to weather server', undefined)
     } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      const { temperature, feelslike, weather_descriptions } = body.current
-      callback(undefined, `${weather_descriptions}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
+      const { temperature, feelslike, weather_descriptions, wind_speed, wind_dir, pressure } = body.current
+      callback(undefined, `${weather_descriptions}. It is currently ${temperature} degrees out. 
+      It feels like ${feelslike} degrees out. 
+      Wind speed is ${wind_speed} from ${wind_dir} direction.
+      Pressure ${pressure}`)
       // callback(undefined, { temperature, feelslike, weather_descriptions })
     }
 
