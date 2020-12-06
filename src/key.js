d65a300192ea47f59fdd205e9bd95a9a105f31fd
przemskydev <przemekgithub@o2.pl>
2020-12-06 19:42:19 +0100

    fix some error

---
+++ src/key.js
@@ -0,0 +1,3 @@
+const key = '9949c9bba6a54400edd8d6102bd2c09f'
+const gelocation = 'pk.eyJ1IjoicHJ6ZW1za3lkZXYiLCJhIjoiY2todWp4dGZpMWRpMDJxcGJvZzJyMmpudSJ9.IawTeRIv3D88DhUlC1gESA'
+module.exports = { key, gelocation }
